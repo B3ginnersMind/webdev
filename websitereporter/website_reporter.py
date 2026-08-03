863c3837fdc132ea2487e1ce89707c8f77107156
Manfred Koerkel <git@feodor.de>
2026-08-03 18:15:10 +0200

    website_reporter version number updated

--- websitereporter/website_reporter.py
+++ websitereporter/website_reporter.py
@@ -13,7 +13,7 @@ import wr.joomla as j
 import wr.mediawiki as m
 import wr.wordpress as w
 import wr.drupal as d
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 if platform.system() != 'Linux':
     print(f"Skript does only support Linux. Exiting...")
